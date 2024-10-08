a321002940a1731b01d0fd339b1c12b2ea5618e2
Victor dS <victordespirlet@gmail.com>
2024-10-08 20:27:24 +0200

    Add files via upload

---
+++ Infinite-Scroll/App.jsx
@@ -0,0 +1,14 @@
+import List from "./components/List"
+
+
+function App() {
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      <div className="max-w-4xl mx-auto">
+        <List />
+      </div>
+    </div>
+  )
+}
+
+export default App
